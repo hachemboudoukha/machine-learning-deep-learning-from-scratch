17f0bc4d485ae15e6cbb6557bf9f609b99c46bc8
hachembou <hachemboudoukha12@gmail.com>
2025-04-08 00:24:48 +0200

    RandomForest from scratch

--- machine_learning/Linear_Regression/LR_train.py
+++ machine_learning/Linear_Regression/LR_train.py
@@ -1,5 +1,5 @@
 import numpy as np
-from machine_learning.linear_regression import my_LinearRegression
+from linear_regression import my_LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
